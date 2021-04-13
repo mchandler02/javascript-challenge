d8e272263cf02c4f7fa171e396c5ce676d5daccc
Molly Sullivan <mchandler02@gmail.com>
2021-04-12 21:21:36 -0500

    started button code

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// console.log("app.js is loaded")
+var button = d3.select('#filter-btn');
+button.on("click", runEnter);
+function runEnter() {
+    d3.event.preentDefault();
+    var inputElement = d3.select('#datetime');
+    var inputValue = inputElement.property('value')
+    console.log(inputValue);
+    
+}
