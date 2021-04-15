f24a56584ae6e6d8c3453741884fa97ae0d7fb08
Molly Sullivan <mchandler02@gmail.com>
2021-04-14 20:48:17 -0500

    data added to table

--- static/js/app.js
+++ static/js/app.js
@@ -3,12 +3,27 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // console.log("app.js is loaded")
-var button = d3.select('#filter-btn');
-button.on("click", runEnter);
-function runEnter() {
-    d3.event.preentDefault();
-    var inputElement = d3.select('#datetime');
-    var inputValue = inputElement.property('value')
-    console.log(inputValue);
-    
-}
+var tbody = d3.select("tbody");
+console.log(data)
+
+data.forEach(function(sightings){
+    console.log(sightings);
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+});
+});
+
+
+
+
+
+// var button = d3.select('#filter-btn');
+// button.on("click", runEnter);
+// function runEnter() {
+//     d3.event.preentDefault();
+//     var inputElement = d3.select('#datetime');
+//     var inputValue = inputElement.property('value')
+//     console.log(inputValue);
