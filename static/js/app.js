ae2f02171d34faed03027a4f70138012166b4a05
Molly Sullivan <mchandler02@gmail.com>
2021-04-16 16:42:47 -0500

    all code working

--- static/js/app.js
+++ static/js/app.js
@@ -19,16 +19,25 @@ UFOdata.forEach(function(sightings){
 
 var button = d3.select('#filter-btn');
 var form = d3.select('#form');
+var inputField = d3.select('#datetime')
 button.on("click", runEnter);
 form.on("submit", runEnter);
+inputField.on("input", runEnter);
 function runEnter() {
     d3.event.preventDefault();
     var inputElement = d3.select('#datetime');
     var inputValue = inputElement.property('value');
     // console.log(inputValue);
     // console.log(UFOdata);
-    var filteredSightings = UFOdata.filter(sighting => sighting.datetime == inputValue);
-    console.log(filteredSightings)
+    if (inputValue === ""){
+        var filteredSightings = UFOdata
+    }
+    else {
+        var filteredSightings = UFOdata.filter(sighting => sighting.datetime == inputValue);
+    }
+    
+    console.log(`Here are my filters: ${filteredSightings}`)
+    
     tbody.html("");
     filteredSightings.forEach(function(sightings){
         var filteredRow = tbody.append("tr");
@@ -38,3 +47,13 @@ function runEnter() {
         });
     });
 };
+
+// UFOdata.forEach(function(sightings){
+//     // console.log(sightings);
+//     var row = tbody.append("tr");
+//     Object.entries(sightings).forEach(function([key, value]) {
+//         // console.log(key, value);
+//         var cell = row.append("td");
+//         cell.text(value);
+// });
+// });
