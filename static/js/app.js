105967c3dc3511cddee06319c4994ae6dcd72b58
Molly Sullivan <mchandler02@gmail.com>
2021-04-16 15:59:46 -0500

    filtering working in table

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,8 @@ var UFOdata = data;
 var tbody = d3.select("tbody");
 // console.log(data)
 
-data.forEach(function(sightings){
+
+UFOdata.forEach(function(sightings){
     // console.log(sightings);
     var row = tbody.append("tr");
     Object.entries(sightings).forEach(function([key, value]) {
@@ -17,15 +18,23 @@ data.forEach(function(sightings){
 });
 
 var button = d3.select('#filter-btn');
-var form = d3.select('#form')
+var form = d3.select('#form');
 button.on("click", runEnter);
-form.on("submit", runEnter)
+form.on("submit", runEnter);
 function runEnter() {
     d3.event.preventDefault();
     var inputElement = d3.select('#datetime');
     var inputValue = inputElement.property('value');
     // console.log(inputValue);
     // console.log(UFOdata);
-    var filteredSightings = UFOdata.filter(sighting => sighting.datetime === inputValue);
+    var filteredSightings = UFOdata.filter(sighting => sighting.datetime == inputValue);
     console.log(filteredSightings)
-}
+    tbody.html("");
+    filteredSightings.forEach(function(sightings){
+        var filteredRow = tbody.append("tr");
+        Object.entries(sightings).forEach(function([key, value]){
+            var filteredCell = filteredRow.append("td");
+            filteredCell.text(value);
+        });
+    });
+};
